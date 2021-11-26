0bab5bab89aab2b5520e6a67867a5282a6e56c48
nabroleonx <nabroleonx@gmail.com>
2021-11-26 23:05:18 +0300

    Added logout endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import StudentRegisterationAPIView, StudentLoginAPIView, StudentAPIView
+from .views import StudentLogoutAPIView, StudentRegisterationAPIView, StudentLoginAPIView, StudentAPIView
 
 app_name = 'users'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', StudentLoginAPIView.as_view(), name="login_student"),
     path('student/', StudentAPIView.as_view(), name="get_student"),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('blacklist/', StudentLogoutAPIView.as_view(), name='blacklist')
 ]
