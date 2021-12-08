1179b26d65014babad37ac4ce1a2dfb5740363b6
earthcomfy <hanagetbel93@gmail.com>
2021-12-08 16:32:12 +0300

    5 - Create and register question and question category models

--- qa/admin.py
+++ qa/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import QuestionCategory, Question
 
-# Register your models here.
+
+admin.site.register(QuestionCategory)
+admin.site.register(Question)
