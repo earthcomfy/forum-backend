b47372ff323fca2626bd1ef79b78b897ffc53749
earthcomfy <hanagetbel93@gmail.com>
2022-01-05 10:23:34 +0300

    12 - Add endpoints to crud answer

--- qa/views.py
+++ qa/views.py
@@ -2,9 +2,9 @@ from rest_framework import status
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Question
+from .models import Question, Answer
 from .permissions import IsUserAuthor
-from .serializers import QuestionSerializer
+from .serializers import QuestionSerializer, AnswerModelSerializer
 
 
 class QuestionListAPIView(generics.ListAPIView):
@@ -25,4 +25,25 @@ class QuestionCreateAPIView(generics.CreateAPIView):
 class QuestionAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    permission_classes = (IsUserAuthor,)
+
+
+class AnswerListAPIView(generics.ListAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerModelSerializer
+
+
+class AnswerCreateAPIView(generics.CreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerModelSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        author = self.request.user
+        serializer.save(author=author)
+
+
+class AnswerAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerModelSerializer
     permission_classes = (IsUserAuthor,)
