c2ab0ab8043059d632dbd4d8c2b111642619bed6
Hannah <66206865+earthcomfy@users.noreply.github.com>
2022-04-14 22:46:06 +0300

    Add comment model for question and answer

--- qa/admin.py
+++ qa/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Answer, QuestionCategory, Question
+from .models import Answer, QuestionCategory, Question, QuestionComment, AnswerComment
 
 
 admin.site.register(QuestionCategory)
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(QuestionComment)
+admin.site.register(AnswerComment)
